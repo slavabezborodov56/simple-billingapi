15941e7a26e204f7275e45396a43fab2d1ad29f8
Vyacheslav Bezborodov <v.bezborodov@cian.ru>
2021-07-19 10:28:42 +0300

    Добавил входную точку CLI

---
+++ src/simple_billingapi/cli.py
@@ -0,0 +1,16 @@
+import click
+
+from simple_billingapi import setup
+
+
+@click.group()
+def cli() -> None:
+    setup()
+
+
+@cli.command()
+@click.option('--debug', is_flag=True)
+@click.option('--host', type=str, default='127.0.0.1')
+@click.option('--port', type=int, default=8000)
+def serve(debug: bool, host: str, port: int) -> None:
+    print('serve')
